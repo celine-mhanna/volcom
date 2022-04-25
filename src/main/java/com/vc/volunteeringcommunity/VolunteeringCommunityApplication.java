346416e5e05043f98fb8f019a239cbdec6569c64
celine <celinemhanna10@gmail.com>
2022-04-25 20:36:56 +0300

    Initial commit

---
+++ src/main/java/com/vc/volunteeringcommunity/VolunteeringCommunityApplication.java
@@ -0,0 +1,14 @@
+package com.vc.volunteeringcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteeringCommunityApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VolunteeringCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vc/volunteeringcommunity/VolunteeringCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vc.volunteeringcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteeringCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
